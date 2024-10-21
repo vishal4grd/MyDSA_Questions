c4519602bac088ebc86b980fb70f1c4ab7c9691b
visha <vishal4grd@gmail.com>
2024-10-21 16:29:07 +0530

    My first Commit

--- DSA-Problems/src/main/java/today/streamapi/problems/StreamApiQuestions.java
+++ DSA-Problems/src/main/java/today/streamapi/problems/StreamApiQuestions.java
@@ -7,7 +7,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-public class SteramApiQuestions {
+public class StreamApiQuestions {
     public static void main(String[] args) {
 
         List<Employee> employees = new ArrayList<>();
