3fea2bd66f9d8fdff7aa07723c31a6cd51f9cf09
visha <vishal4grd@gmail.com>
2024-10-26 18:09:17 +0530

    added lombok dependency

--- DSA-Problems/src/main/java/today/pojo/Employee.java
+++ DSA-Problems/src/main/java/today/pojo/Employee.java
@@ -6,6 +6,7 @@ import lombok.Data;
 @Data
 @AllArgsConstructor
 public class Employee {
+    private int id;
     private String name;
     private double salary;
     private int age;
--- DSA-Problems/src/main/java/today/streamapi/problems/StreamApiQuestions.java
+++ DSA-Problems/src/main/java/today/streamapi/problems/StreamApiQuestions.java
@@ -2,50 +2,88 @@ package today.streamapi.problems;
 
 import today.pojo.Employee;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class StreamApiQuestions {
     public static void main(String[] args) {
 
         List<Employee> employees = new ArrayList<>();
-        employees.add(new Employee("John Doe", 50000.0, 30, "IT", "Software Engineer", "123 Main St"));
-        employees.add(new Employee("Jane Smith", 60000.0, 28, "Finance", "Accountant", "456 Elm St"));
-        employees.add(new Employee("Bob Johnson", 70000.0, 35, "Marketing", "Manager", "789 Oak St"));
-        employees.add(new Employee("Alice Brown", 40000.0, 25, "HR", "HR Manager", "901 Maple St"));
-        employees.add(new Employee("Mike Davis", 55000.0, 32, "IT", "Data Analyst", "111 Pine St"));
-        employees.add(new Employee("Emily Chen", 65000.0, 29, "Finance", "Financial Analyst", "222 Cedar St"));
-        employees.add(new Employee("David Lee", 75000.0, 38, "Marketing", "Marketing Manager", "333 Spruce St"));
-        employees.add(new Employee("Sarah Taylor", 45000.0, 26, "HR", "HR Generalist", "444 Fir St"));
-        employees.add(new Employee("Kevin White", 60000.0, 31, "IT", "IT Manager", "555 Willow St"));
-        employees.add(new Employee("Lisa Nguyen", 70000.0, 36, "Finance", "Financial Manager", "666 Oak St"));
+        employees.add(new Employee(1, "John Doe", 50000.0, 30, "IT", "Software Engineer", "123 Main St"));
+        employees.add(new Employee(10, "Jane Smith", 60000.0, 28, "Finance", "Accountant", "456 Elm St"));
+        employees.add(new Employee(4, "Bob Johnson", 70000.0, 35, "Marketing", "Manager", "789 Oak St"));
+        employees.add(new Employee(3, "Alice Brown", 40000.0, 25, "HR", "HR Manager", "901 Maple St"));
+        employees.add(new Employee(5, "Mike Davis", 55000.0, 32, "IT", "Data Analyst", "111 Pine St"));
+        employees.add(new Employee(7, "Emily Chen", 65000.0, 29, "Finance", "Financial Analyst", "222 Cedar St"));
+        employees.add(new Employee(6, "David Lee", 75000.0, 38, "Marketing", "Marketing Manager", "333 Spruce St"));
+        employees.add(new Employee(9, "Sarah Taylor", 45000.0, 26, "HR", "HR Generalist", "444 Fir St"));
+        employees.add(new Employee(8, "Kevin White", 60000.0, 31, "IT", "IT Manager", "555 Willow St"));
+        employees.add(new Employee(2, "Lisa Nguyen", 70000.0, 36, "Finance", "Financial Manager", "666 Oak St"));
+        employees.add(new Employee(2, "Lisa Nguyen", 70000.0, 36, "Finance", "Financial Manager", "666 Oak St"));
 
         //Find list of employees whose name starts with alphabet A
 
         List<Employee> aEmp = employees.stream().filter(e -> e.getName().startsWith("A") || e.getName().startsWith("a")).collect(Collectors.toList());
-        System.out.println(aEmp.get(0).getName());
+//        System.out.println(aEmp.get(0).getName());
 
         //Group The employees By Department Names
 //        Map<String, List<Employee>> deptMap = employees.stream().collect(Collectors.groupingBy(emp -> emp.getDepartment()));
         Map<String, List<Employee>> deptMap = employees.stream().collect(Collectors.groupingBy(Employee::getDepartment)); // or we can replace the lambda expn by method reference
         for (Map.Entry<String, List<Employee>> entry : deptMap.entrySet()) {
             for (Employee emp : entry.getValue()) {
-                System.out.println(emp.getDepartment());
+//                System.out.println(emp.getDepartment());
             }
         }
-        //Find the max age of employees
+        //Find the max age of employees6
 //        int maxAge = employees.stream().mapToInt(emp -> emp.getAge()).max().getAsInt();
         int maxAge = employees.stream().mapToInt(Employee::getAge).max().getAsInt();
-        System.out.println("Max age is :  " + maxAge);
+//        System.out.println("Max age is :  " + maxAge);
 
 //        Find all department names
         List<String> deptNames = employees.stream().map(Employee::getDepartment).collect(Collectors.toList());
-        System.out.println(deptNames);
+//        System.out.println(deptNames);
 
 //        Find the count of employee in each department
-        Map<String, Long> listCount = employees.stream().collect(Collectors.groupingBy(Employee::getDepartment, Collectors.counting()));
-        System.out.println("Count of employee in each department :  " + listCount);
+//        Map<String, Long> listCount = employees.stream().collect(Collectors.groupingBy(Employee::getDepartment, Collectors.counting()));
+        Map<String, List<Employee>> listCount = employees.stream().collect(Collectors.groupingBy(Employee::getDepartment));
+//        System.out.println("Count of employee in each department :  " + listCount);
+        for (Map.Entry<String, List<Employee>> emp : listCount.entrySet()) {
+//                System.out.println("Key: " + emp.getKey() + "value " +  emp.getValue());
+        }
+
+        List<Employee> sortedEmplist = employees.stream().sorted(Comparator.comparing(Employee::getName)).collect(Collectors.toList());
+//        sortedEmplist.forEach(System.out::println);
+
+        // sort with name and age with no duplicate
+        List<Employee> sortedEmplistNameandSalary = employees.stream().distinct().sorted(Comparator.comparing(Employee::getId).thenComparing(Employee::getName)).collect(Collectors.toList());
+//        sortedEmplistNameandSalary.forEach(System.out::println);
+
+        //sort employee in reverse order
+        List<Employee> sortNameRevOrder = employees.stream().distinct().sorted(Comparator.comparing(Employee::getName).reversed()).collect(Collectors.toList());
+//        sortNameRevOrder.forEach(System.out::println);
+
+        // print List of employee age in reverse order
+        List<Employee> empListAgeRevOrder = employees.stream().sorted(Comparator.comparing(Employee::getAge).reversed()).collect(Collectors.toList());
+//        empListAgeRevOrder.forEach(System.out::println);
+
+        // Print the highest salary employee in each department
+
+        Map<String , Employee> res = employees.stream().collect(Collectors.groupingBy(Employee::getDepartment, Collectors.collectingAndThen(Collectors.maxBy(Comparator.comparingDouble(Employee::getSalary)), Optional::get)));
+//        res.entrySet().forEach(e -> System.out.println("Department :" + e.getKey() + "          Salary :" + e.getValue()));
+
+        // Find department wise counting of employee
+        Map<String, Long> deptWiseCounting = employees.stream().collect(Collectors.groupingBy(Employee::getDepartment, Collectors.counting()));
+//        deptWiseCounting.entrySet().forEach(e -> System.out.println("Department : " + e.getKey() +"   Number of employees : " + e.getValue()));
+//        deptWiseCounting.forEach((key, value) -> System.out.println("Department : " + key + "   Number of employees : " + value));
+//        deptWiseCounting.forEach((key, value) -> System.out.printf("Department : %s , Number of Employees : %d%n", key, value));
+
+//        Find the department name which has the highest number of employee
+        Map<String, Long> deptNameHighSalary  = employees.stream().collect(Collectors.groupingBy(Employee::getDepartment, Collectors.counting()));
+        String depps =  deptNameHighSalary.entrySet().stream().max(Map.Entry.comparingByValue()).get().getKey();
+//        System.out.println(" Department Name with Highest salary : " + depps);
+
+//      Find the department names that these employees work for , where the number in employees in the department is over 3.
+        Map<String, Long> deptNameHighSalary1  = employees.stream().collect(Collectors.groupingBy(Employee::getDepartment, Collectors.counting()));
+        deptNameHighSalary1.entrySet().stream().filter(e -> e.getValue() > 3).forEach(System.out::println);
     }
 }
